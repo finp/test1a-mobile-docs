efe96a0ec582be46e1861e57e8d11ffbadebdeeb
Paul Wright <pwright@redhat.com>
2018-01-30 12:29:39 +0000

    init

---
+++ script.sh
@@ -0,0 +1,16 @@
+cd temp
+rm -rf *
+wget https://github.com/finp/test1-android-sdk-docs/archive/master.zip
+wget https://github.com/finp/test1-android-sdk-docs/archive/v1.0.zip
+wget https://github.com/finp/test1-ios-sdk-docs/archive/master.zip
+wget https://github.com/finp/test1-ios-sdk-docs/archive/v1.zip
+wget https://github.com/finp/test1-push-docs/archive/master.zip
+wget https://github.com/finp/test1-push-docs/archive/v1.0.zip
+wget https://github.com/finp/test1-sync-docs/archive/master.zip
+wget https://github.com/finp/test1-sync-docs/archive/v1.0.zip
+
+for f in *.zip*
+do
+  unzip -o $f
+done
+rm *zip*
